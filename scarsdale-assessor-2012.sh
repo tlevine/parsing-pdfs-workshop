e6d4a097531c16213f65169a8873deb4c15edcc6
Thomas Levine <_@thomaslevine.com>
2014-04-02 20:02:19 +0000

    convert python to sed

--- scarsdale-assessor-2012.sh
+++ scarsdale-assessor-2012.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+fn="scarsdale-assessor-2012.txt"
 
-grep '[ 0-9.]{8,10}\*\**$' "$1"
+sed -n 's/^\**\([ 0-9.]\{8,10\}\) \**$/\1/p' "$fn"
 #   data['ACCT'] = int(re.findall(r'ACCT\: ([0-9]+)', text)[0])
 #   data['CD'] = re.findall(r'\n([A-Z]{2})\n', text)[0]
