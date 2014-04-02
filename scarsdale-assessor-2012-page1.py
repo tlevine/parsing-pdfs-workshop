537801200eb3288aa994bed45544524d8307528b
Thomas Levine <_@thomaslevine.com>
2014-04-02 20:14:57 +0000

    python

---
+++ scarsdale-assessor-2012-page1.py
@@ -0,0 +1,13 @@
+# Pretty way
+import lxml.etree
+svg = lxml.etree.parse('scarsdale-assessor-2012-page1.svg')
+svg.xpath('//svg:g', namespaces = {'svg':'http://www.w3.org/2000/svg'})
+
+# Ugly way
+import lxml.html
+svg = lxml.html.parse('scarsdale-assessor-2012-page1.svg')
+svg.xpath('//g')
+
+# Let's use the pretty way. It turns out that there are three nested groups,
+# as we can see inside the Inkscape GUI.
+svg.xpath('//svg:g/svg:g/svg:g', namespaces = {'svg':'http://www.w3.org/2000/svg'})
