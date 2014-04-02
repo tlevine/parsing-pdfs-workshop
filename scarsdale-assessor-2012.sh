0cb29f39f574d7555991a439a318134710eaf76d
Thomas Levine <_@thomaslevine.com>
2014-04-02 20:03:24 +0000

    more notes

--- scarsdale-assessor-2012.sh
+++ scarsdale-assessor-2012.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+set -e
 fn="scarsdale-assessor-2012.txt"
 
 sed -n 's/^\**\([ 0-9.]\{8,10\}\) \**$/\1/p' "$fn"
 #   data['ACCT'] = int(re.findall(r'ACCT\: ([0-9]+)', text)[0])
 #   data['CD'] = re.findall(r'\n([A-Z]{2})\n', text)[0]
+
+pdf2svg "$fn"
