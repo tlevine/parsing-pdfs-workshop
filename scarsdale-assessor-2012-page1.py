d29c2ab4d207299d283e0842c734823b5728cc9d
Thomas Levine <_@thomaslevine.com>
2014-04-02 21:41:48 +0000

    parse scarsdale

--- scarsdale-assessor-2012-page1.py
+++ scarsdale-assessor-2012-page1.py
@@ -10,7 +10,24 @@ svg.xpath('//g')
 
 # Let's use the pretty way. It turns out that there are three nested groups,
 # as we can see inside the Inkscape GUI.
+svg = lxml.etree.parse('scarsdale-assessor-2012-page1.svg')
 svg.xpath('//svg:g/svg:g/svg:g', namespaces = {'svg':'http://www.w3.org/2000/svg'})
 svg.xpath('//svg:g/svg:g/svg:g/*', namespaces = {'svg':'http://www.w3.org/2000/svg'})
 svg.xpath('//svg:g/svg:g/svg:g/svg:text/*', namespaces = {'svg':'http://www.w3.org/2000/svg'})
-svg.xpath('count(//svg:g/svg:g/svg:g/svg:text/*)', namespaces = {'svg':'http://www.w3.org/2000/svg'})
+print(svg.xpath('count(//svg:g/svg:g/svg:g/svg:text/*)', namespaces = {'svg':'http://www.w3.org/2000/svg'}))
+
+def rows(svg):
+    texts = svg.xpath('//svg:g/svg:g/svg:g/svg:text/svg:tspan/text()', namespaces = {'svg':'http://www.w3.org/2000/svg'})
+    thisrow = []
+    print(texts)
+    for text in texts:
+        thisrow.append(text)
+        if '***' in text and thisrow != []:
+            yield thisrow
+            thisrow = []
+
+import struct
+def cleanrow(row):
+    pass #01.01.3 OX 210 1 FAMILY RES COUNTY TAXABLE 20,200 
+
+print(list(rows(svg)))
