fcf020fa8046c362a2cd031564321ebd37f00ccf
Thomas Levine <_@thomaslevine.com>
2014-10-21 11:20:00 +0000

    generate slides

--- slides.py
+++ slides.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
+import os
 import sys
 from functools import partial
 
 def main():
     sys.stdout.write(header)
     for directory in ['public-notice','scarsdale-assessor','prizren-zoning']:
+        sys.stdout.write('\sectionslide{%s}\n' % directory)
         makefile = os.path.join(directory, 'Makefile')
         with open(makefile) as fp:
             for title, command in makefile_to_slides(fp):
-                sys.stdout.write('\slide{%s}{%s}' % (title, command))
+                sys.stdout.write('\slide{%s}{%s}\n' % (title, command))
     sys.stdout.write(footer)
 
 def makefile_to_slides(fp):
@@ -22,10 +24,21 @@ def makefile_to_slides(fp):
 
 header = r'''
 \documentclass[a4paper,landscape]{article}
-\newcommand\slide
+\newcommand\sectionslide[1] {
+  \newpage
+  \section{#1}
+}
+\newcommand\slide[2] {
+  \newpage
+  \subsection{#1}
+  \texttt{#2}
+}
 \begin{document}
 '''
 
 footer = r'''
 \end{document}
 '''
+
+if __name__ == '__main__':
+    main()
